8e8018fdf4460bbfb49427f7aa364393926742dc
Gabriel Pureliani <gafureliani@gmail.com>
2023-08-26 16:43:33 +0400

    feat: Add query keys and cache
    - Read query data from query cache if an entry with that specific key exists.
    - Fix the "isLoading" variable not changing state

--- src/query.ts
+++ src/query.ts
@@ -1,8 +1,9 @@
-import { createSignal } from "solid-js";
+import { createComputed, createEffect, createSignal, onMount } from "solid-js";
 import type { Accessor } from "solid-js";
 
 export type QueryOptions<Response, Error> = {
     queryFn: () => Promise<Response>
+    key?: Accessor<string>
     enabled?: () => boolean
     onSuccess?: (data: Response) => void
     onError?: (error: Error) => void
@@ -20,12 +21,21 @@ export type CreateQueryReturn<Response, Error> = {
 
 export function createQuery<Response = unknown, Error = unknown>(options: QueryOptions<Response, Error>): CreateQueryReturn<Response, Error>
 export function createQuery<Response, Error>(options: QueryOptions<Response, Error>): CreateQueryReturn<Response, Error> {
+    const cache: Record<string, Response> = {}
+
     const [data, setData] = createSignal<Response | undefined>(undefined);
     const [error, setError] = createSignal<Error | undefined>(undefined);
-
     const [isError, setIsError] = createSignal<boolean>(false);
     const [isLoadingInitial, setIsLoadingInitial] = createSignal<boolean>(false);
     const [isLoading, setIsLoading] = createSignal<boolean>(false);
+
+    const clear = () => {
+        setError(undefined);
+        setData(undefined);
+        setIsError(false);
+        setIsLoadingInitial(false);
+        setIsLoading(false);
+    };
 
     const enabled = () => {
         if (options.enabled) {
@@ -36,12 +46,19 @@ export function createQuery<Response, Error>(options: QueryOptions<Response, Err
 
     const refetch = async (): Promise<Response | undefined> => {
         if(!enabled()) return;
- 
+
         try {
+            setIsLoading(true)
             const _data = await options.queryFn();
             setData(() => _data);
             options.onSuccess?.(_data);
             setIsError(false);
+
+            // update cache
+            if(options?.key) {
+                cache[options.key()] = _data;
+            }
+
             return _data;
         } catch (e) {
             setError(() => e as Error);
@@ -59,13 +76,22 @@ export function createQuery<Response, Error>(options: QueryOptions<Response, Err
         });
     }
 
-    const clear = () => {
-        setError(undefined);
-        setData(undefined);
-        setIsError(false);
-        setIsLoadingInitial(false);
-        setIsLoading(false);
-    };
+    let firstRun = true
+    createEffect(() => {
+        const key = options.key?.();
+        if (!key) return;
+
+        if (firstRun) {
+            firstRun = false;
+        } else {
+            if (key in cache) {
+                clear();
+                setData(() => cache[key]);
+            } else {
+                refetch();
+            }
+        }
+    })
 
     return {
         data,
