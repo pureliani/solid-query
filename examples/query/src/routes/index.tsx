a784b09e5a00b0bac3c8da2af634b19f859748b8
Gabriel Pureliani <gafureliani@gmail.com>
2023-08-27 14:03:02 +0400

    refactor: query example

--- examples/query/src/routes/index.tsx
+++ examples/query/src/routes/index.tsx
@@ -1,8 +1,10 @@
-import { createMutation, createQuery } from "@gapu/solid-query"
+import { createQuery } from "@gapu/solid-query"
 import axios from "axios"
 import { Show, createSignal } from "solid-js"
 import { LoadingDots, LoadingScreen } from "~/components/LoadingScreen"
 import { SomethingWentWrongScreen } from "~/components/SomethingWentWrongScreen"
+
+const [postId, setPostId] = createSignal(1)
 
 type QueryResponse = {
     id: number
@@ -10,7 +12,6 @@ type QueryResponse = {
     body: string
     userId: number
 }
-const [postId, setPostId] = createSignal(1)
 
 export const { 
     data, 
@@ -19,40 +20,18 @@ export const {
     isLoading, 
     isLoadingInitial, 
     refetch, 
-    clear 
+    clear,
+    setData,
+    setError
 } = createQuery<QueryResponse>({
-    key: () => String(postId()),
+    key: () => postId(),
     queryFn: async () => {
         const { data: post } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId()}`);
         return post;
     }
 });
 
-type MutationRequest = {
-    title: string
-    body: string
-    userId: number
-}
-
-type MutationResponse = {
-    id: number
-    title: string
-    body: string
-    userId: number
-}
-
-export const { isLoading: isMutating, mutate: createPost } = createMutation<MutationRequest, MutationResponse>({
-    mutationFn: async (body) => {
-        const { data } = await axios.post("https://jsonplaceholder.typicode.com/posts", body);
-        return data;
-    },
-    onSuccess() {
-      refetch()
-    },
-});
-
 export default function Home() {
-
     const onNext = () => setPostId(current => current + 1)
     const onPrev = () => setPostId(current => current - 1)
 
